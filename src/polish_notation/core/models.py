11dd3e775df6af17623cd6ef6858bc155d57aef6
ArekkusuDev <arekkusu.dev@outlook.com>
2025-10-04 23:33:55 -0600

    feat: add abstract syntax tree (AST) node classes for expression evaluation

---
+++ src/polish_notation/core/models.py
@@ -0,0 +1,40 @@
+from typing import Union
+
+
+class ASTNode:
+    pass
+
+
+class BinaryOp(ASTNode):
+    def __init__(self, left: ASTNode, op: str, right: ASTNode):
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def __repr__(self) -> str:
+        return f"BinaryOp({self.left}, {self.op}, {self.right})"
+
+
+class UnaryOp(ASTNode):
+    def __init__(self, op: str, operand: ASTNode):
+        self.op = op
+        self.operand = operand
+
+    def __repr__(self) -> str:
+        return f"UnaryOp({self.op}, {self.operand})"
+
+
+class Number(ASTNode):
+    def __init__(self, value: Union[int, float]):
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"Number({self.value})"
+
+
+class Identifier(ASTNode):
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"Identifier({self.name})"
